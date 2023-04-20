650fc740e7de6c0381d0a94ec25f44aff932a14a
MarkusMagpie <118437885+MarkusMagpie@users.noreply.github.com>
2023-04-20 21:32:43 +0700

    сделал films датабазу, аккуратно

--- data/users.py
+++ data/users.py
@@ -14,7 +14,7 @@ class User(SqlAlchemyBase, UserMixin):
     email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
 
-    news = orm.relationship("News", back_populates='user')
+    films = orm.relationship("Film", back_populates='user')
 
     def __repr__(self):
         return f'<User> {self.id} {self.name} {self.email}'
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request, abort, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 from forms.news import NewsForm
+from forms.films import FilmsForm
 from forms.user import RegisterForm, LoginForm
 from data.news import News
 from data.users import User
@@ -35,25 +36,24 @@ def main():
 @app.route('/news', methods=['GET', 'POST'])
 @login_required
 def add_news():
-    form = NewsForm()
+    form = FilmsForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        news = News()
+        news = Film()
         news.title = form.title.data
         news.content = form.content.data
-        news.is_private = form.is_private.data
-        current_user.news.append(news)
+        current_user.films.append(news)
         db_sess.merge(current_user)
         db_sess.commit()
         return redirect('/')
-    return render_template('news.html', title='Добавление новости', form=form)
+    return render_template('news.html', title='Добавление рецензии на фильм', form=form)
 
 
 @app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
 @login_required
 def news_delete(id):
     db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+    news = db_sess.query(Film).filter(Film.id == id, Film.user == current_user).first()
     if news:
         db_sess.delete(news)
         db_sess.commit()
@@ -65,28 +65,26 @@ def news_delete(id):
 @app.route('/news/<int:id>', methods=['GET', 'POST'])
 @login_required
 def edit_news(id):
-    form = NewsForm()
+    form = FilmsForm()
     if request.method == "GET":
         db_sess = db_session.create_session()
-        news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+        news = db_sess.query(Film).filter(Film.id == id, Film.user == current_user).first()
         if news:
             form.title.data = news.title
             form.content.data = news.content
-            form.is_private.data = news.is_private
         else:
             abort(404)
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
+        news = db_sess.query(Film).filter(Film.id == id, Film.user == current_user).first()
         if news:
             news.title = form.title.data
             news.content = form.content.data
-            news.is_private = form.is_private.data
             db_sess.commit()
             return redirect('/')
         else:
             abort(404)
-    return render_template('news.html', title='Редактирование новости', form=form)
+    return render_template('news.html', title='Редактирование рецензии на фильм', form=form)
 
 
 @app.route("/")
