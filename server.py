bb3296ae270f13535a252ed2c1ede5703f10f4ba
NikitaLikhotvorik <79364198+NikitaLikhotvorik@users.noreply.github.com>
2023-04-24 16:49:03 +0700

    Update server.py

--- server.py
+++ server.py
@@ -164,6 +164,11 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     main()
 #aaa
